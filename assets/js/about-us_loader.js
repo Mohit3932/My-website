305329992bf0ff12629ebc524698151c6c50c7b5
Mohit <110518231+Mohit3932@users.noreply.github.com>
2022-09-12 12:01:50 +0530

    Add files via upload

---
+++ assets/js/about-us_loader.js
@@ -0,0 +1,14 @@
+
+window.onload=()=>{
+
+    setTimeout(()=>{
+         document.getElementById('loader').style.width='0';
+         
+     },2000)
+   
+    setTimeout(()=>{
+       
+         document.getElementById('loading').style.display='none';
+     },4000)
+   
+}
