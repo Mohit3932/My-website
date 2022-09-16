afb6530e3a5eb30b99345720a3121e54a1626274
Mohit <110518231+Mohit3932@users.noreply.github.com>
2022-09-16 18:27:25 +0530

    Add files via upload

---
+++ assets/js/image_change.js
@@ -0,0 +1,25 @@
+
+function changeBackgroundImage(){
+document.getElementById('Myimage').style.backgroundroundimage="url(../assets/media/artist2.jpg)";
+
+if(document.rightbar){
+
+   document.rightbar.background= 
+}
+
+
+
+
+
+    // alert("Warning message.");
+   
+    // image.addEventListener('click',function(){
+    // 	changeBackgroundImage();
+    // });
+
+    // function changeBackgroundImage()
+    // {
+    // 	image.src='../assets/media/artist2.jpg';
+    // }
+
+}
